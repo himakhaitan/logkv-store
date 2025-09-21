bc488995238f88ae3753661cd638608474a5ef84
Shivam Verma <shivamv1332@gmail.com>
2025-09-22 03:37:47 +0530

    Basic server command implementation for Windows

--- cli/commands/server.go
+++ cli/commands/server.go
@@ -1,6 +1,10 @@
 package commands
 
 import (
+	"fmt"
+	"os/exec"
+	"runtime"
+
 	"github.com/himakhaitan/logkv-store/cli/output"
 	"github.com/spf13/cobra"
 )
@@ -11,9 +15,35 @@ func NewServerCommand() *cobra.Command {
 		Use:   "server",
 		Short: "Start the LogKV server",
 		Run: func(cmd *cobra.Command, args []string) {
+
+			if runtime.GOOS != "windows" {
+				output.Info(fmt.Sprintf("Server commands are not implemented for %v in CLI mode.", runtime.GOOS))
+				output.Info("Use 'logkvd' binary to start the server.")
+				return
+			}
+
 			output.Info("Starting LogKV server...")
-			output.Info("Server commands are not implemented in CLI mode.")
-			output.Info("Use 'logkvd' binary to start the server.")
+			if err := buildServer(); err != nil {
+				output.Error(fmt.Sprintf("Error in building the Server: %v", err))
+				return
+			}
+			output.Info("Server Built successfully.")
+
+			if err := startServer(); err != nil {
+				output.Error(fmt.Sprintf("Error in starting the Server: %v", err))
+				return
+			}
+			output.Info("Server started successfully")
 		},
 	}
+}
+
+func buildServer() error {
+	cmd := exec.Command("go", "build", "-o", "./bin/logkvd.exe", "./cmd/logkvd/main.go")
+	return cmd.Start()
+}
+
+func startServer() error {
+	cmd := exec.Command("cmd", "/C", "start", "./bin/logkvd.exe")
+	return cmd.Start()
 }
